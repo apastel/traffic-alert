e0d9c494f7df7d03ac1703c82aebe7fff1c6dabb
Alex Pastel <alex.pastel@moesol.com>
2020-01-07 22:19:40 -0800

    Second more notifications as commute time decreases

--- src/server.js
+++ src/server.js
@@ -16,6 +16,27 @@ const store = {}
 const app = express()
 app.use(express.json())
 
+const withinCommuteTimeWindow = (d1, d2, timeZone) => {
+    const now = new Date(new Date().toLocaleString('en-US', { timeZone }))
+    return now >= d1 && now <= d2
+}
+
+const commuteHasDecreasedSincePreviousNotification = (triggerIdentity, durationInTraffic) => {
+    const amountDecreased = 5 // minutes
+    if (!store[triggerIdentity].lastNotifiedDuration) {
+        console.log(`${triggerIdentity}: No prior notification for this time window`)
+        console.log(`${JSON.stringify(store)}`)
+        return true
+    }
+    if (durationInTraffic <=
+        store[triggerIdentity].lastNotifiedDuration - (amountDecreased * store[triggerIdentity].thresholdMultiplier)) {
+        console.log(`${triggerIdentity}: Commute decreased at least ${amountDecreased} minutes since last notification`)
+        return true
+    }
+    console.log(`${triggerIdentity}: Commute has not decreased by ${amountDecreased} min since last notification at ${store[triggerIdentity].lastNotifiedDuration}`)
+    return false
+}
+
 // The status
 app.get('/', (req, res) => {
     res
@@ -27,6 +48,10 @@ app.get('/', (req, res) => {
 // The status
 app.get('/ifttt/v1/status', serviceKeyCheck, (req, res) => {
     res.status(200).send()
+})
+
+app.get('/data', (req, res) => {
+    res.status(200).send(JSON.stringify(store, null, 2))
 })
 
 // The test/setup endpoint
@@ -53,35 +78,37 @@ app.post(
     '/ifttt/v1/triggers/threshold_reached',
     serviceKeyCheck,
     (req, res) => {
-        const data = []
         const triggerIdentity = req.body.trigger_identity
-        if (!Object.keys(store).includes(triggerIdentity)) {
-            store[triggerIdentity] = {
-                thresholdMultiplier: 0
-            }
-        }
         const originAddress = req.body.triggerFields.origin_address
         const destinationAddress = req.body.triggerFields.destination_address
         const thresholdDuration = parseInt(
             req.body.triggerFields.threshold_duration, 10
         )
-        const startWindow = req.body.triggerFields.commute_window_start
-        const endWindow = req.body.triggerFields.commute_window_end
+        const windowStart = req.body.triggerFields.commute_window_start
+        const windowEnd = req.body.triggerFields.commute_window_end
         const timeZone = req.body.user.timezone
         const d1 = new Date(new Date().toLocaleString('en-US', { timeZone }))
-        d1.setHours(parseInt(startWindow))
+        d1.setHours(parseInt(windowStart))
         d1.setMinutes(0)
+        d1.setSeconds(0)
         const d2 = new Date(new Date().toLocaleString('en-US', { timeZone }))
-        d2.setHours(parseInt(endWindow))
+        d2.setHours(parseInt(windowEnd))
         d2.setMinutes(0)
+        d2.setSeconds(0)
+        if (!Object.keys(store).includes(triggerIdentity)) {
+            store[triggerIdentity] = {
+                thresholdMultiplier: 0,
+                timeZone,
+                windowStart: d1,
+                windowEnd: d2
+            }
+        }
         console.log(`trigger_identity: ${triggerIdentity}`)
-        const now = new Date(new Date().toLocaleString('en-US', { timeZone }))
-        if (now < d1 || now > d2) {
+        const data = []
+        if (!withinCommuteTimeWindow(d1, d2, timeZone)) {
             console.log('Not within commute time window.')
             store[triggerIdentity].thresholdMultiplier = 0
-            res.status(200).send({
-                data
-            })
+            res.status(200).send({ data })
             return
         }
         console.log(
@@ -100,22 +127,29 @@ app.post(
                     response.json.routes[0].legs[0].duration_in_traffic.value / 60
                 )
                 console.log(`Summary: ${summary}`)
-                console.log(`Duration in traffic: ${durationInTraffic}`)
+                console.log(`Commute time: ${durationInTraffic}`)
                 console.log(`Threshold duration: ${thresholdDuration}`)
-                if (durationInTraffic <= thresholdDuration - 5 * store[triggerIdentity].thresholdMultiplier) {
-                    console.log('threshold reached...')
-                    store[triggerIdentity].thresholdMultiplier++
-                    data.push({
-                        commute_duration: durationInTraffic,
-                        origin_address: originAddress,
-                        destination_address: destinationAddress,
-                        route_to_take: summary,
-                        created_at: new Date().toISOString(), // Must be a valid ISOString
-                        meta: {
-                            id: generateUniqueId(),
-                            timestamp: Math.floor(Date.now() / 1000) // This returns a unix timestamp in seconds.
-                        }
-                    })
+
+                if (durationInTraffic <= thresholdDuration) {
+                    console.log(`${triggerIdentity}: Commute is below threshold`)
+                    if (commuteHasDecreasedSincePreviousNotification(triggerIdentity, durationInTraffic)) {
+                        console.log('Generating trigger data')
+                        store[triggerIdentity].lastNotifiedDuration = durationInTraffic
+                        store[triggerIdentity].thresholdMultiplier++
+                        data.push({
+                            commute_duration: durationInTraffic,
+                            origin_address: originAddress,
+                            destination_address: destinationAddress,
+                            route_to_take: summary,
+                            created_at: new Date().toISOString(), // Must be a valid ISOString
+                            meta: {
+                                id: generateUniqueId(),
+                                timestamp: Math.floor(Date.now() / 1000) // This returns a unix timestamp in seconds.
+                            }
+                        })
+                    }
+                } else {
+                    delete store[triggerIdentity].lastNotifiedDuration
                 }
 
                 res.status(200).send({
@@ -129,30 +163,46 @@ app.post(
     }
 )
 
+app.delete('/ifttt/v1/triggers/threshold_reached/trigger_identity/:triggerId', serviceKeyCheck, (req, res) => {
+    const triggerIdentity = req.params.triggerId
+    if (store[triggerIdentity]) {
+        delete store[triggerIdentity]
+        console.log(`Deleted trigger identity ${triggerIdentity}`)
+    } else {
+        console.log(`Trigger identity ${triggerIdentity} not found in database`)
+    }
+    res.status(200).send()
+})
+
 const listener = app.listen(process.env.PORT, () => {
     console.log(`Your app is listening on port ${listener.address().port}`)
 })
 
 function enableRealtimeAPI () {
     Object.keys(store).forEach((triggerIdentity) => {
-        post('https://realtime.ifttt.com/v1/notifications',
-            {
-                data: [{
-                    trigger_identity: triggerIdentity
-                }]
-            }, {
-                headers: {
-                    'Content-Type': 'application/json',
-                    'IFTTT-Service-Key': IFTTT_KEY
+        console.log(`${triggerIdentity}: Checking if within commute window for Realtime API`)
+        if (withinCommuteTimeWindow(store[triggerIdentity].windowStart, store[triggerIdentity].windowEnd, store[triggerIdentity].timeZone)) {
+            post('https://realtime.ifttt.com/v1/notifications',
+                {
+                    data: [{
+                        trigger_identity: triggerIdentity
+                    }]
+                }, {
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'IFTTT-Service-Key': IFTTT_KEY
+                    }
                 }
-            }
-        )
-            .then((response) => {
-                console.log(`Notified IFTTT to poll trigger_identity ${triggerIdentity}`)
-            })
-            .catch((error) => {
-                console.error(error)
-            })
+            )
+                .then((response) => {
+                    console.log(`Notified IFTTT to poll trigger_identity ${triggerIdentity}`)
+                })
+                .catch((error) => {
+                    console.error(error)
+                })
+        } else {
+            console.log(`${triggerIdentity}: Not within commute window for Realtime API`)
+        }
     })
 }
 
